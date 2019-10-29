e70eb3af0140f75c181f9ce5392d5ee5c0ebac00
Arminditya Fajri Akbar <arminditya.akbar@bhinneka.com>
2019-10-30 03:38:17 +0700

    codeginiter testing 3

--- src/application/config/routes.php
+++ src/application/config/routes.php
@@ -51,7 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'login';
 $route['404_override'] = 'Handler404';
-//$route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['email'] = 'Email_Controller';
 
