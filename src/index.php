257ff3dbbe7293cf2c4b8e0e40e0aa2ff82e8407
Arminditya Fajri Akbar <arminditya.akbar@bhinneka.com>
2019-10-30 02:12:22 +0700

    bugs 2

--- src/index.php
+++ src/index.php
@@ -2,21 +2,23 @@
 
 $db = parse_url(getenv("DATABASE_URL"));
 
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
+try{
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
 
-echo "HERE"; die;
+    $sql = "SELECT * FROM users";
+    $query = $pdo->prepare($sql);
+    $rows = $query->execute();
 
-$sql = "SELECT * FROM users";
-$query = $pdo->prepare($sql);
-$rows = $query->execute();
-
-print_r($rows->fetch());
+    print_r($rows->fetch());
+} catch(PDOException $e) {
+    print_r($e->getMessage());
+}
 
 ?>
