47683b42f8bb5f9bbe5cbfc94a2b6bc283362b44
Arminditya Fajri Akbar <arminditya.akbar@bhinneka.com>
2019-11-08 19:30:58 +0700

    init user table migration

--- db/migrations/20191103173744_create_users_table.php
+++ db/migrations/20191103173744_create_users_table.php
@@ -7,8 +7,13 @@ class CreateUsersTable extends AbstractMigration
     public function change()
     {
         $table = $this->table('users');
-        $table->addColumn('user_id', 'integer')
-            ->addColumn('created_at', 'datetime')
+        $table->addColumn('email', 'string', ['limit' => 100])
+            ->addColumn('password', 'string', ['limit' => 64])
+            ->addColumn('first_name', 'string', ['limit' => 128])
+            ->addColumn('last_name', 'string', ['limit' => 128])
+            ->addColumn('created', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('modified_at', 'datetime', ['null' => true])
+            ->addIndex(['username', 'email'], ['unique' => true])
             ->create();
     }
 }
